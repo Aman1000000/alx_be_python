e74b9aa65e7662008c750bd6e564695156c2c94d
Aman1000000 <yimeramanuel2@gmail.com>
2025-05-21 11:06:57 +0300

    basic_operation\

---
+++ basic_operation.py
@@ -0,0 +1,12 @@
+number1= 10
+number2=5
+Addition = "Addition of " + str(number1) + " and "+ str(number2)+ " is " +str(number1+number2) +"\n"
+Substraction= "Substraction of " + str(number1) + " and "+ str(number2)+ " is " +str(number1-number2) + "\n"
+Product= "Product of " + str(number1) + " and "+ str(number2)+ " is " +str(number1*number2) + "\n"
+result= Addition + Substraction + Product
+print(result)
+
+
+
+
+
