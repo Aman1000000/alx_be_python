ae339cf1e3a86896fd906186713522ae541ff26c
Aman1000000 <yimeramanuel2@gmail.com>
2025-05-21 11:19:25 +0300

    minor

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,4 +1,4 @@
-number1= 10
+number1=10
 number2=5
 Addition = "Addition of " + str(number1) + " and "+ str(number2)+ " is " +str(number1+number2) +"\n"
 Substraction= "Substraction of " + str(number1) + " and "+ str(number2)+ " is " +str(number1-number2) + "\n"
