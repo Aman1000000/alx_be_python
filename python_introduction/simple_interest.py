be09ce8908a160994d275c03fcdcbc6a0eb38b2e
Aman1000000 <yimeramanuel2@gmail.com>
2025-05-21 12:09:42 +0300

    spelling

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,6 +1,6 @@
-princpal = 1000
-rate = 0.05
-time = 3
+princpals = 1000
+rates = 0.05
+times = 3
 interest= princpal * rate * time
 result = "The simple interest is: " + str( interest)
 print (result)
