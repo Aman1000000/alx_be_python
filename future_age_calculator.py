04e9321082916b0466cd82b0bb631e8afac8faea
Aman1000000 <yimeramanuel2@gmail.com>
2025-05-21 11:55:33 +0300

    finishing

---
+++ future_age_calculator.py
@@ -0,0 +1,3 @@
+age = int(input("How old are you? ")) + 27 
+result = "In 2050, you will be " + str(age) + " years old." 
+print(result)
---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,4 @@
+hours = 2
+second = 7200
+result = str(hours) + " hour(s) is " + str(second) + " seconds." 
+print (result)
