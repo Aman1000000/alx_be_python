7bca6de6419d9c7f9c278fdaefb1fd8036f01e73
Aman1000000 <yimeramanuel2@gmail.com>
2025-05-21 13:18:45 +0300

    spell

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,6 +1,6 @@
-princpals = 1000
-rates = 0.05
-times = 3
-interest= princpals * rates * times
+principal = 1000
+rate = 0.05
+time = 3
+interest= principal * rate * time
 result = "The simple interest is: " + str( interest)
 print (result)
