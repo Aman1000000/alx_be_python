c61dad2179f765c654054702a27dd77c2e0dca0f
Aman1000000 <yimeramanuel2@gmail.com>
2025-05-27 14:20:57 +0300

    pattern

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,11 @@
+row = 0
+counter = 0
+asterisk = "*"
+
+while counter < 4:
+    row = 0  
+    while row < 4:
+        print(asterisk, end = " ")  
+        row += 1
+    print()  
+    counter += 1
