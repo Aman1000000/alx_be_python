45901067f768b776251edb97110220d1b1df6a80
Aman1000000 <yimeramanuel2@gmail.com>
2025-05-27 16:18:39 +0300

    small change

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -3,10 +3,14 @@ while loop <= 100:
  task = input ( "Enter your task: " )
  priority = input ("Priority (high/medium/low): ")
  time = input ("Is it time-bound? (yes/no): ")
+ match priority:
+   case  meduim:
+    medium = " "
+   
  if time == "yes":
   loop = True
   print (f"{task} is a {priority} priority task, that requires immediate attention today! ")
  elif time == "no":
-  print( (f"{task} is a {priority} priority task. Consider completing it when you have free time. ") )
+  print( (f"{task} is a {priority} priority task. Consider completing it when you have free time.") )
   
   
