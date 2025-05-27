3daa07e4cf86c788d183bed7a7ba551d7f60e5af
Aman1000000 <yimeramanuel2@gmail.com>
2025-05-27 15:03:19 +0300

    change

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,10 +1,10 @@
 x = int(input ("Enter the first number: "))
 y = int(input("Enter the second number: "))
 
-Z = input(" Choose the operation (+, -, *, /):")
+operation = input("Choose the operation (+, -, *, /): ")
 if y == 0:
  print("Cannot divide by zero.")
-match Z:
+match operation:
     case "+":
         print ("The result is " + str (x + y) + ".")
     case "-":
