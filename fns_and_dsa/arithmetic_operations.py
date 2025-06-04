47503cddd846f428e7202dc66e8a5584acfc3aad
Aman1000000 <yimeramanuel2@gmail.com>
2025-06-04 14:13:30 +0300

    fix

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,26 +1,23 @@
 num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: ")) 
-operation = input("Enter the operation (add, substract, multiply, divide): ").strip().lower()
-
+num2 = float(input("Enter the second number: "))
+operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 def perform_operation(num1, num2, operation):
-    match operation:
-        case "add":
-            return num1 + num2
-        case "substract":  
-            return num1 - num2
-        case "multiply":
-            return num1 * num2
-        case "divide":
-            if num2 != 0:
-                return num1 / num2
-            else:
-                return "Cannot divide by zero"
-        case _:
-            return "Invalid operation entered."
-        
-result = perform_operation(num1, num2, operation)
-print(result)
-
+ match operation:
+  case "add":
+   print (num1 + num2 )
+  case "substract":
+   print (num1 - num2 )
+  case "multiply":
+   print (num1 * num2 )
+  case "divide":
+   if num2 != 0:
+    print (num1 / num2 )
+   elif num2 == 0 :
+   print ("can not divide by zero")
+  case _:
+   print("Invalid data type entered.")
+ return perform_operation
+perform_operation(num1 ,num2 , operation) 
    
     
      
