d925c5ca1bb6dedee898588ace3601e9d0e0d082
Aman1000000 <yimeramanuel2@gmail.com>
2025-06-04 13:56:00 +0300

    paramater_change

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,8 +1,7 @@
 num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
+num2 = float(input("Enter the second number: ")) 
 operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
-def perform_operation():
-
+def perform_operation(num1,num2,operation):
  match operation:
   case "add":
     print (num1 + num2 )
@@ -19,7 +18,8 @@ def perform_operation():
 
   case _:
         print("Invalid data type entered.")
-perform_operation()
+
+perform_operation(num1,num2,operation)
    
     
      
