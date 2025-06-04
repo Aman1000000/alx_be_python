2a4203ddb6b476925dd3f247fab3228990d03100
Aman1000000 <yimeramanuel2@gmail.com>
2025-06-04 13:43:35 +0300

    operation_code

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,19 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+def perform_operation():
+ match operation:
+  case "add":
+    print (num1 + num2 )
+  case "substract":
+    print (num1 - num2 )
+  case "multiply":
+    print (num1 * num2 )
+  case "divide":
+    print (num1 / num2 )
+  case _:
+        print("Invalid data type entered.")
+perform_operation()
+   
+    
+     
