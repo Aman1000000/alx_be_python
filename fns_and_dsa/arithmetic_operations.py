1073d2cead2fe04176889a4b3a1bc7f2678de48a
Aman1000000 <yimeramanuel2@gmail.com>
2025-06-04 13:57:49 +0300

    space_on_the_parameter

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,7 +1,7 @@
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: ")) 
 operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
-def perform_operation(num1,num2,operation):
+def perform_operation(num1, num2, operation):
  match operation:
   case "add":
     print (num1 + num2 )
